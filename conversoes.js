627d06cb57b072870f8ccc8b79b8dbbeb0cac0b7
Peffmb <pedroferreiirazzz@gmail.com>
2022-05-11 09:25:22 -0300

    conversões

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dados
+// booleanos
+
+// conversão implícita
+const numero = 456;
+// const numeroString = ("456a");
+
+
+
+//number()
+//string()
+console.log(numero === numeroString) // false não é igual
+console.log(numero + numeroString) // soma porém não os valores
+console.log(numero == numeroString) // true porque converte tipo string para number
+
+
+
+// conversão explícita
